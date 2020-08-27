f2f2a45501ad056a7b3e819ecc6ac25e7873c720
Jose Maria Perez <jm.perez@globant.com>
2020-08-27 13:49:44 -0300

    Ejercicio2

--- legajosDeColegio.ts
+++ legajosDeColegio.ts
@@ -1,46 +1,73 @@
 class Colegio{
-    private Alumnos: string[]=[];
-    private DarDealtaAlumno:boolean;
-    private PromedioDeNota: number;
+    private AdministradorDeAlumnos: string;
+    private DarDealtaAlumnos: string;
+    private Promedio: number;
     private Nota: number;
-    private Suma=0;
+    private Suma: number;
 
 
-    public constructor(Alumno: [], PromedioDeNota: number,DarDealtaAlumno, Nota: number){
-         this.Alumnos=[];     
-         this.PromedioDeNota=0;
-         this.DarDealtaAlumno=true;
+    public constructor(){     
+         this.Promedio=0;
+         this.DarDealtaAlumnos="";
          this.Nota=10;
          this.Suma=0;
+         this.AdministradorDeAlumnos="";
     }
+
+
+    //* este medoto agregar a cada alumno 
+    public setAdminitrarAlumnos(AdministradorDeAlumnos: string): void{
+        this.AdministradorDeAlumnos=AdministradorDeAlumnos;
+    }
+
+
+    public getAdminitrarAlumnos(): string{
+        return this.AdministradorDeAlumnos;
+    }
+
+
     /*
-    public setCargarAlumnos = ([]) =>{
-        for(let i =0; i<[].length;i++){
-            console.log([]);
+    public ObtenerPromedioDelAlumno(): void{
+        for(let i=0; i<this.Nota;i++){
+            if(this.Promedio==this.Suma/this.Nota)
+                this.Promedio= this.Promedio+this.Nota;
         }
-    }*/
-    public setNotasPromedio = (number) =>{
+    }
+    */
+
+
+    
+    public setPromedio = (number) =>{
         for(let i= 0; i<this.Nota; i++){
-            if(this.Suma=this.Nota/this.PromedioDeNota){
-                this.PromedioDeNota;
+            if(this.Promedio){
+                this.Promedio=this.Promedio/this.Nota;
             }
-            return this.PromedioDeNota;
+            return this.Promedio;
         }
     }
-    public ObtenerPromedioInscritos = (string) =>{
-        for(let i=0; i<[].length; i++){
-            string = this.ObtenerPromedioInscritos; 
+    
+    /*
+    public getPromedio(): number{
+        return this.Promedio;
+    }
+    */
+    
+    public setObtenerPromedioInscritos = (string) =>{
+        for(let i=0; i<this.AdministradorDeAlumnos.length; i++){
+            if(this.AdministradorDeAlumnos)
+            this.AdministradorDeAlumnos+= this.Suma/this.Promedio;
         }
-        return  this.ObtenerPromedioInscritos;
+        return this.setObtenerPromedioInscritos;
     }
 
-    public setDarAlumnoDeAlta(DarDealtaAlumno: true):void{
-        if(this.PromedioDeNota>=7)
-        this.DarDealtaAlumno=true;
-        else 
-        this.DarDealtaAlumno=false;
+
+    public setDarAlumnoDeAlta(DarDealtaAlumnos: string):void{
+        this.DarDealtaAlumnos=DarDealtaAlumnos;
     }
 
+    public gtAlumnoDeAlta(): string{
+        return this.DarDealtaAlumnos;
+    }
 
 
     /*
@@ -48,15 +75,16 @@ class Colegio{
         this.Alumnos=Alumnos;
     }*/
 
+    /*
     public getAlumnos(): []{
         return [];
-    }
+    }*/
 
 }
 
 
 
-class Alumnos extends Colegio{
+class Alumno extends Colegio{
     private nombre: string;
     private apellido: string;
     private edad: number;
@@ -64,9 +92,10 @@ class Alumnos extends Colegio{
     private nacionalidad: string;
     private localidad: string;
 
+    
 
-    public constructor(Alumno: [], PromedioDeNota: number,DarDealtaAlumno: boolean, Nota: number){
-        super(Alumno,PromedioDeNota,DarDealtaAlumno,Nota);
+    public constructor(){
+        super();
         this.nombre="";
         this.apellido="";
         this.edad=0;
