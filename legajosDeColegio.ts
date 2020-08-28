3b218b366d250c3e35790921afbe6d110fb41d59
Jose Maria Perez <jm.perez@globant.com>
2020-08-28 18:52:04 -0300

    Ejercicio2

--- legajosDeColegio.ts
+++ legajosDeColegio.ts
@@ -1,154 +1,103 @@
+import * as fs from 'fs';
+import * as readlineSync from 'readline-sync';
+
 class Colegio{
-    private AdministradorDeAlumnos: string;
-    private DarDealtaAlumnos: string;
-    private Promedio: number;
-    private Nota: number;
-    private Suma: number;
-
-
-    public constructor(){     
-         this.Promedio=0;
-         this.DarDealtaAlumnos="";
-         this.Nota=10;
-         this.Suma=0;
-         this.AdministradorDeAlumnos="";
-    }
-
-
-    //* este medoto agregar a cada alumno 
-    public setAdminitrarAlumnos(AdministradorDeAlumnos: string): void{
-        this.AdministradorDeAlumnos=AdministradorDeAlumnos;
-    }
-
-
-    public getAdminitrarAlumnos(): string{
-        return this.AdministradorDeAlumnos;
-    }
-
-
-    /*
-    public ObtenerPromedioDelAlumno(): void{
-        for(let i=0; i<this.Nota;i++){
-            if(this.Promedio==this.Suma/this.Nota)
-                this.Promedio= this.Promedio+this.Nota;
-        }
-    }
-    */
-
-
-    
-    public setPromedio = (number) =>{
-        for(let i= 0; i<this.Nota; i++){
-            if(this.Promedio){
-                this.Promedio=this.Promedio/this.Nota;
-            }
-            return this.Promedio;
-        }
-    }
-    
-    /*
-    public getPromedio(): number{
-        return this.Promedio;
-    }
-    */
-    
-    public setObtenerPromedioInscritos = (string) =>{
-        for(let i=0; i<this.AdministradorDeAlumnos.length; i++){
-            if(this.AdministradorDeAlumnos)
-            this.AdministradorDeAlumnos+= this.Suma/this.Promedio;
-        }
-        return this.setObtenerPromedioInscritos;
-    }
-
-
-    public setDarAlumnoDeAlta(DarDealtaAlumnos: string):void{
-        this.DarDealtaAlumnos=DarDealtaAlumnos;
-    }
-
-    public gtAlumnoDeAlta(): string{
-        return this.DarDealtaAlumnos;
-    }
-
-
-    /*
-    let setAlumnos(Alumnos: []): void{
-        this.Alumnos=Alumnos;
-    }*/
-
-    /*
-    public getAlumnos(): []{
-        return [];
-    }*/
-
-}
-
-
-
-class Alumno extends Colegio{
+    private alumnos: number;
     private nombre: string;
     private apellido: string;
-    private edad: number;
-    private FechaDeNacimiento: number;
-    private nacionalidad: string;
-    private localidad: string;
-
+    private fechaDeNacimiento: number;
+    private asistencias: number;
+    private promedio: number;
+    private nota: number;
+    private suma: number;
     
-
-    public constructor(){
-        super();
+      
+    public constructor(){     
+        this.promedio=0;
+        this.fechaDeNacimiento=1/1/1; 
+        this.nota=0; 
+        this.suma=0;
+        this.alumnos=0; 
+        this.asistencias=0; 
         this.nombre="";
-        this.apellido="";
-        this.edad=0;
-        this.FechaDeNacimiento=0;
-        this.nacionalidad="";
-        this.localidad="";
+        this.apellido="";    
     }
     
-    public setNombre(nombre: string): void{
+
+
+
+    public setNombre(nombre:string):void{
         this.nombre=nombre;
     }
 
-    public getNombre(): string{
+
+    public getNombre():string{
         return this.nombre;
     }
-
-    public setApellido(apellido: string): void{
+    public setApellido(apellido:string):void{
         this.apellido=apellido;
     }
 
     public getApellido(): string{
         return this.apellido;
     }
-    public setEdad(edad: number): void{
-        this.edad=edad;
+    public setAdminitrarAlumnos(alumnos: number): void{
+        this.alumnos=alumnos;
+    }
+    public getAdminitrarAlumnos(): number{
+        return this.alumnos;
+    }
+    public setAsistencias(asistencias: number): void{
+        this.asistencias=asistencias;
     }
 
-    public getEdad(): number{
-        return this.edad;
+    public getAsistencias(): number{
+        return this.asistencias;
     }
 
-    public setFechaDeNacimiento(FechaDeNacimiento: number): void{
-        this.FechaDeNacimiento=FechaDeNacimiento;
+    public setFechaDeNacimiento(fechaDeNacimiento:number):void{
+        this.fechaDeNacimiento=fechaDeNacimiento;
     }
 
-    public getFechaDeNacimiento(): number{
-        return this.FechaDeNacimiento;
+    public getFecheDeNacimiento():number{
+        return this.fechaDeNacimiento;
     }
 
-    public setNacionalidad(nacionalidad: string): void{
-        this.nacionalidad=nacionalidad;
+    public setNotas(nota: number): void{
+        this.nota=nota;
     }
 
-    public getNacionalidad(): string{
-        return this.nacionalidad;
+    public getNotas(): number{
+        return this.nota;
     }
 
-    public setLocalidad(localidad: string): void{
-        this.localidad=localidad;
+    public setPromedioAlumno(nota:number): number{
+        for(let i=0; i<this.nota; i++){
+        this.promedio=this.suma/this.nota+this.asistencias;
+        }
+        return this.promedio;
     }
 
-    public getLocalidad(): string{
-        return this.localidad;
+    public setPromedioDeAlumnosInscriptos(alumnos:number):number{
+        for(let i=0; this.alumnos;i++){
+            this.promedio = this.suma/this.alumnos;
+        }
+        return this.promedio;
     }
 
+    public setDarAlumnosDeAlta(alumnos: number): void{
+        this.alumnos=alumnos;
+    }
+
+    public getDarAlumnosDeAlta(): number{
+        return this.alumnos;
+    }
 }
+
+let colegio = new Colegio();
+colegio.setNombre("franco");
+colegio.setNombre("morales");
+colegio.setFechaDeNacimiento(19/1/1999);
+colegio.setPromedioAlumno(9);
+
+console.log(colegio);
